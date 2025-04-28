7c8e7606516f43016644ec7569aeac945a283113
tuanbao <bao2732003@gmail.com>
2025-04-29 01:00:34 +0700

    create router

--- App.tsx
+++ App.tsx
@@ -1,13 +1,25 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from "./components/Home";
+import ResultScreen from "./components/Result";
+import ScoreScreen from "./components/Score";
+import StatisticScreen from 'components/Statistic';
+import InfoScreen from "./components/Info";
+import ReviewScreen from "./components/Review";
 
-import './global.css';
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx" />
-      <StatusBar style="auto" />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Result" component={ResultScreen} />
+        <Stack.Screen name="Score" component={ScoreScreen} />
+        <Stack.Screen name="Review" component={ReviewScreen} />
+        <Stack.Screen name="Statistic" component={StatisticScreen} />
+        <Stack.Screen name="Info" component={InfoScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ components/Result.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, View, Text } from 'react-native';
+import React from 'react';
+
+const ResultScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Result Screen</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ResultScreen;
